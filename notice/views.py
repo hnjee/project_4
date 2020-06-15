f9089ac52ea66d8ee8d145ba1fd313df6233033e
hnjee <leehnjee@gmail.com>
2020-06-15 16:49:01 +0900

    data transfer

--- notice/views.py
+++ notice/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
  
 def list(request):
-    return render(request, 'notice/list.html')
+    #Data 전달
+    context = {'board':'notice', 'age':20}
+    return render(request, 'notice/list.html', context)
 
 def write(request):
     return render(request, 'notice/write.html')
